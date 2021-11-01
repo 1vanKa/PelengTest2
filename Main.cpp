ce11d514d1065f469b5d67c05acb55fe03ff2923
1vanKa <ivan.karpovich@gmail.com>
2021-11-01 15:58:08 +0300

    Cleanup main

--- Main.cpp
+++ Main.cpp
@@ -35,18 +35,11 @@ int main(int argc, char** argv) {
 		Point(1054,98), Point(1167,70), Point(1189,32), Point(1160,0) };
 	double PI = atan(1.0) * 4;
 	Camera camera(60.0 / 180 * PI, 40.0 / 180 * PI, -10.0 / 180 * PI, -37.0 / 180 * PI, 1920, 1080, 3);
-	std::vector<Point> convertedPoints;
 	for (Point p : points) {
 		double x = 0;
 		double y = 0;
-		CoordinateConverter::convertPerspectiveToMetricalCoordinates(&x, &y, p.getX(), p.getY(), camera);
-		convertedPoints.push_back(Point(x, y));
+		CoordinateConverter::convertImagePointToMetricalCoordinates(&x, &y, p.getX(), p.getY(), camera);
 		std::cout << '[' << p.getX() << ',' << p.getY() << "] -> " << '[' << x << ',' << y << "]" << std::endl;
 	}
-	double x = 0;
-	double y = 0;
-	Camera camera1(60.0 / 180 * PI, 40.0 / 180 * PI, -0 / 180 * PI, -37.0 / 180 * PI, 1920, 1080, 3);
-	CoordinateConverter::convertPerspectiveToMetricalCoordinates(&x, &y, 1920, 1080, camera1);
-	std::cout << x << ';' << y;
 	return 0;
 }
